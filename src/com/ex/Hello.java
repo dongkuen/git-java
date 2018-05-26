0c97d7fcf8c01e805f35c549d5c33ef8f01a7354
dongkuen <rucro@naver.com>
2018-05-26 15:33:17 +0900

    c0

--- src/com/ex/Hello.java
+++ src/com/ex/Hello.java
@@ -4,17 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("HelloWorld!");
-		System.out.println("c0");
-		System.out.println("c1");
-		System.out.println("c2");
-		System.out.println("c3");
-		System.out.println("develop");
-		System.out.println("develop2");
-		System.out.println("develop3");
-		System.out.println("develop4");
-		System.out.println("topic");
-		System.out.println("topic2");
+		
 		
 	}
 	
